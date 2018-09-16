bf88de305998bf8ccdcfbb71e9a7b1c8dae0775e
bpdesilva <buwanekapasan@gmail.com>
2018-09-17 02:12:19 +0530

    Init
    First Demo

---
+++ BottomNav/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet
+} from "react-native";
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
+import Icon from 'react-native-vector-icons/Ionicons'
+class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome to React Native !</Text>
+      </View>
+    );
+  }
+}
+class SettingsScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Settings Tabs</Text>
+      </View>
+    );
+  }
+}
+
+
+export default createMaterialBottomTabNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ios-home" color={tintColor} size={24} />
+      )
+    }
+  },
+  Settings: {
+    screen: SettingsScreen,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ios-settings" color={tintColor} size={24} />
+      )
+    }
+  }
+}, {
+    initialRouteName: 'Home',
+    activeTintColor: 'white',
+    shifting: true
+  })
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
